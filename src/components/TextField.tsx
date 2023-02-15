5c28e7dbf2b9ffdc98fecde8bed9ae9c844e6e0b
Lucas Barallobre <79661802+lucasbara@users.noreply.github.com>
2023-02-15 20:05:30 -0300

    Add textfield

---
+++ src/components/TextField.tsx
@@ -0,0 +1,25 @@
+import { AiOutlineSearch } from 'react-icons/ai';
+
+type InputProps = {
+  name: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder: string;
+};
+
+export function TextField({ name, onChange, placeholder }: InputProps) {
+  return (
+    <form className="flex items-center bg-grey h-10 w-[20%] text-white placeholder-light-grey p-2 rounded-lg  focus:outline-none focus:ring focus:ring-violet-300">
+      <label htmlFor="search">
+        <AiOutlineSearch className="w-7" />
+      </label>
+      <input
+        className="w-full bg-grey focus:outline-none"
+        id="search"
+        name={name}
+        onChange={onChange}
+        placeholder={placeholder}
+        type="search"
+      />
+    </form>
+  );
+}
