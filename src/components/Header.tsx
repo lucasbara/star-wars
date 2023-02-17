41ffc151ac497681e66a71b626e83481788848d6
Lucas Barallobre <79661802+lucasbara@users.noreply.github.com>
2023-02-17 16:22:12 -0300

    Add link to Home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
+import { Link } from 'react-router-dom';
 import StarsWarsLogoSVG from '@/assets/images/star-wars-logo.png';
-import { TextField } from '@/components/TextField';
 
 export function Header() {
   return (
-    <header className="w-full flex justify-between items-center">
-      <img alt="Logo | Star Wars" className="max-w-[150px] fill-red" src={StarsWarsLogoSVG} />
-      <TextField name="search" placeholder="Search for a character..." />
+    <header className="w-full flex justify-center items-center">
+      <Link to="/">
+        <img alt="Logo | Star Wars" className="max-w-[150px] fill-red" src={StarsWarsLogoSVG} />
+      </Link>
     </header>
   );
 }
