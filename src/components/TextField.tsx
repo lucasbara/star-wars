a0324455d3e4fcea0b214949baa0daa939373c40
Lucas Barallobre <79661802+lucasbara@users.noreply.github.com>
2023-02-17 19:10:19 -0300

    Add value as a prop

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -4,11 +4,12 @@ type InputProps = {
   name: string;
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
   placeholder: string;
+  value: string;
 };
 
 export function TextField({ name, onChange, placeholder }: InputProps) {
   return (
-    <form className="flex items-center bg-grey h-10 w-[20%] text-white placeholder-light-grey p-2 rounded-lg  focus:outline-none focus:ring focus:ring-violet-300">
+    <form className="flex items-center bg-grey h-10 w-[20%] text-white placeholder-light-grey p-2 rounded-lg  focus:outline-none">
       <label htmlFor="search">
         <AiOutlineSearch className="w-7" />
       </label>
