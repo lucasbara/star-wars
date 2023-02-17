54db1b35716735ca50a0b8da13abab7f0fbbc3ed
Lucas Barallobre <79661802+lucasbara@users.noreply.github.com>
2023-02-17 19:36:45 -0300

    Add min width

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -15,7 +15,7 @@ export function Button({ children, disabled, onClick, type }: ButtonProps) {
   return (
     <button
       className={classNames(
-        'h-10 w-[10%] flex justify-between items-center mx-2 text-white bg-grey border-violet hover:bg-violet p-2 rounded-lg cursor-pointer border transition duration-300 ease-out',
+        'h-10 min-w-[10rem] w-[10%] flex justify-between items-center mx-2 text-white bg-grey border-violet hover:bg-violet p-2 rounded-lg cursor-pointer border transition duration-300 ease-out',
         {
           'bg-light-grey cursor-default border-light-grey hover:bg-light-grey': disabled === true,
         },
