05a970e9176de241e54eac6f1e62136a94fd01bf
Lucas Barallobre <79661802+lucasbara@users.noreply.github.com>
2023-02-17 17:04:03 -0300

    Add new colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,8 +5,11 @@ module.exports = {
     colors: {
       black: '#161618',
       grey: '#232323',
+      'hover-grey': '#78D0D3',
       'light-grey': '#a0a0a0',
+      violet: '#9e8cfc',
       white: '#FFFFFF',
+      'white-darker': '#ecedee',
     },
   },
   plugins: [],
