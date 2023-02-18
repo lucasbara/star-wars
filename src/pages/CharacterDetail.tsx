40f2f4f9a72a2d756da0ae7cc76c89e0083d8779
Lucas Barallobre <79661802+lucasbara@users.noreply.github.com>
2023-02-18 18:51:14 -0300

    Place "Go Back" buton on the bottom

--- src/pages/CharacterDetail.tsx
+++ src/pages/CharacterDetail.tsx
@@ -21,14 +21,14 @@ export function CharacterCardDetail() {
   return (
     <Layout>
       <section className="w-full h-full flex flex-col ">
+        <CharacterCard character={character} version="detail" />
         <Link
-          className="h-10 w-[10%] flex justify-between items-center mx-2 text-white bg-grey border-violet hover:bg-violet p-2 rounded-lg cursor-pointer border transition duration-300 ease-out"
+          className="h-10 w-[10%] flex justify-between items-center mx-2 text-white bg-grey border-violet hover:bg-violet p-2 ml-5 rounded-lg cursor-pointer border transition duration-300 ease-out"
           to="/"
         >
           <MdKeyboardArrowLeft className="text-xl mr-2" />
           Go back
         </Link>
-        <CharacterCard character={character} version="detail" />
       </section>
     </Layout>
   );
