82170407ea08bce30bf317ee7e1329a6a602ace5
Lucas Barallobre <79661802+lucasbara@users.noreply.github.com>
2023-02-18 14:13:00 -0300

    Add onChange property

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,15 +1,23 @@
 type SelectProps = {
+  onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
   options: Array<any>;
+  placeholder: string;
+  value: string;
 };
 
-export function Select({ options }: SelectProps) {
+export function Select({ onChange, options, placeholder, value }: SelectProps) {
   return (
-    <select className="bg-grey w-[20%] h-10 text-white placeholder-light-grey p-2 rounded-lg  focus:outline-none mx-2">
-      <option value="1">Select a...</option>
-      {options.map((option, idx) => {
-        const { title } = option;
+    <select
+      className="bg-grey w-[20%] h-10 text-white placeholder-light-grey p-2 rounded-lg focus:outline-none mx-2"
+      value={value}
+      onChange={onChange}
+    >
+      <option value={placeholder}>{placeholder}</option>
+      <option value="all">All</option>
+      {options.map((option) => {
+        const [url, title] = option;
         return (
-          <option key={title} value={idx}>
+          <option key={url} value={url}>
             {title}
           </option>
         );
