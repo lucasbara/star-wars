5663fa903b2ba8c9cd77edaf82796b922298b12e
Lucas Barallobre <79661802+lucasbara@users.noreply.github.com>
2023-02-18 16:05:29 -0300

    Add characters types

---
+++ src/types/characters.ts
@@ -0,0 +1,32 @@
+export type Character = {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+  homeworld: string;
+  films: Array<string>;
+  species: Array<string>;
+  vehicles: Array<string>;
+  starships: Array<string>;
+  created: string;
+  edited: string;
+  url: string;
+};
+
+export type CharacterApiResponse = {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: Array<Character>;
+};
+
+type CharacterState = {
+  characters: Array<Character>;
+  error: unknown | null;
+  isLoading: boolean;
+  setCharacters: () => void;
+};
