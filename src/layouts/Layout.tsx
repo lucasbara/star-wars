ba75ff5b47cf59d2ce427e57e39c47931762684a
Lucas Barallobre <79661802+lucasbara@users.noreply.github.com>
2023-02-18 15:58:30 -0300

    Add main layout

---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react';
+import { Footer } from '@/components/Footer';
+import { Header } from '@/components/Header';
+
+type LayoutProps = {
+  children: ReactNode;
+};
+
+export function Layout({ children }: LayoutProps) {
+  return (
+    <div className="w-full max-w-[80vw] mx-auto h-screen flex flex-col">
+      <Header />
+      <main className="flex-1 p-6">{children}</main>
+      <Footer />
+    </div>
+  );
+}
