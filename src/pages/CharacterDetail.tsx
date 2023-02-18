fabdeed9829a752e7d61c98ff96dd46e92ce3c93
Lucas Barallobre <79661802+lucasbara@users.noreply.github.com>
2023-02-18 18:25:40 -0300

    Add Go back link

--- src/pages/CharacterDetail.tsx
+++ src/pages/CharacterDetail.tsx
@@ -1,15 +1,10 @@
-import { useState } from 'react';
+import { MdKeyboardArrowLeft } from 'react-icons/md';
+import { Link, useHistory, useParams } from 'react-router-dom';
 import { Layout } from '@/layouts/Layout';
-import { TextField } from '@/components/TextField';
-import { useHistory, useParams } from 'react-router-dom';
 import { CharacterCard } from '@/components/CharacterCard';
 import { useCharactersStore } from '@/store/characters';
 
-let searchTimeout: NodeJS.Timeout;
-
 export function CharacterCardDetail() {
-  const [search, setSearch] = useState('');
-
   const { name } = useParams<{ name: string }>();
 
   const { characters } = useCharactersStore();
@@ -17,14 +12,6 @@ export function CharacterCardDetail() {
   const character = characters.find((character) => character.name === name);
 
   const history = useHistory();
-
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    clearTimeout(searchTimeout);
-
-    searchTimeout = setTimeout(() => {
-      setSearch(event.target.value);
-    }, 500);
-  };
 
   if (!character) {
     history.push('/not-found');
@@ -34,14 +21,13 @@ export function CharacterCardDetail() {
   return (
     <Layout>
       <section className="w-full h-full flex flex-col ">
-        <div className="w-full flex justify-center items-center">
-          <TextField
-            name="search"
-            onChange={onChange}
-            placeholder="Search for a character..."
-            value={search}
-          />
-        </div>
+        <Link
+          className="h-10 w-[10%] flex justify-between items-center mx-2 text-white bg-grey border-violet hover:bg-violet p-2 rounded-lg cursor-pointer border transition duration-300 ease-out"
+          to="/"
+        >
+          <MdKeyboardArrowLeft className="text-xl mr-2" />
+          Go back
+        </Link>
         <CharacterCard character={character} version="detail" />
       </section>
     </Layout>
